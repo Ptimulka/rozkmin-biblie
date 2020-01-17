2e55aeda851a67735474eed4943bede4c0a9d5c7
Marta <ptimuleczka@gmail.com>
2020-01-17 23:06:56 +0100

    Add headings for Wj and Kpl

--- src/bible/content/addons/headings.js
+++ src/bible/content/addons/headings.js
@@ -167,6 +167,349 @@ var headings = {
     [
       { title: "Bracia proszą Józefa o przebaczenie", after: 13 }
     ],
+  ],
+  "Wj": [
+    [
+      { title: "Rozrost i ucisk Izraela", after: 0 }
+    ],
+    [
+      { title: "Urodzenie Mojżesza", after: 0 },
+      { title: "Mojżesz w ziemi Madian", after: 10 },
+      { title: "Pierwszy opis powołania Mojżesza", after: 22 }
+    ],
+    [
+    ],
+    [
+    ],
+    [
+      { title: "Pierwsze spotkanie Mojżesza z faraonem", after: 0 }
+    ],
+    [
+      { title: "Drugi opis powołania Mojżesza", after: 1 }
+    ],
+    [
+      { title: "Plagi egipskie", after: 13 }
+    ],
+    [
+    ],
+    [
+    ],
+    [
+    ],
+    [
+      { title: "Noc paschalna i wyjście z Egiptu", after: 0 }
+    ],
+    [
+    ],
+    [
+      { title: "Cudowne przejście przez morze", after: 16 }
+    ],
+    [
+    ],
+    [
+      { title: "Pieśń dziękczynna", after: 0 },
+      { title: "Pierwsze postoje na pustyni", after: 21 }
+    ],
+    [
+      { title: "Przepiórki i manna", after: 0 }
+    ],
+    [
+      { title: "Wyprowadzenie wody ze skały", after: 0 },
+      { title: "Zwycięstwo nad Amalekitami", after: 7 }
+    ],
+    [
+      { title: "Spotkanie Mojżesza z Jetrą", after: 0 }
+    ],
+    [
+      { title: "Objawienie się Boga Izraelowi", after: 0 }
+    ],
+    [
+      { title: "Dekalog", after: 0 },
+      { title: "Prawo o ołtarzu", after: 21 }
+    ],
+    [
+      { title: "Prawo rodzinne", after: 0 },
+      { title: "Prawo karne", after: 11 },
+      { title: "Prawo rzeczowe", after: 32 }
+    ],
+    [
+      { title: "Prawo rodzinne", after: 14 },
+      { title: "Prawo moralne", after: 16 }
+    ],
+    [
+      { title: "Rok szabatowy", after: 9 },
+      { title: "Święta", after: 13 },
+      { title: "Zachęta do zachowania przepisów Prawa", after: 19 }
+    ],
+    [
+      { title: "Zawarcie przymierza", after: 0 },
+      { title: "Tablice przykazań", after: 11 }
+    ],
+    [
+      { title: "Dary na budowę przybytku", after: 0 },
+      { title: "Przybytek i jego sprzęty. Arka", after: 9 },
+      { title: "Stół chlebów pokładnych i naczynia", after: 22 },
+      { title: "Świecznik", after: 30 }
+    ],
+    [
+      { title: "Przybytek", after: 0 }
+    ],
+    [
+      { title: "Ołtarz całopalenia", after: 9 },
+      { title: "Dziedziniec", after: 8 },
+      { title: "Oliwa do świecznika", after: 19 }
+    ],
+    [
+      { title: "Szaty kapłańskie", after: 0 },
+      { title: "Efod", after: 5 },
+      { title: "Pektorał", after: 14 },
+      { title: "Suknia arcykapłańska", after: 30 },
+      { title: "Diadem na tiarze", after: 35 },
+      { title: "Odmienny ubiór kapłanów", after: 39 }
+    ],
+    [
+      { title: "Konsekracja Aarona i jego synów", after: 0 },
+      { title: "Ofiara ustawiczna", after: 37 }
+    ],
+    [
+      { title: "Inne przepisy o przybytku. Ołtarz kadzenia", after: 0 },
+      { title: "Pogłówne", after: 10 },
+      { title: "Kadź z brązu", after: 16 },
+      { title: "Sporządzanie oleju i kadzidła", after: 21 }
+    ],
+    [
+      { title: "Wykonawcy robót", after: 0 },
+      { title: "Szabat", after: 11 }
+    ],
+    [
+      { title: "Kult złotego cielca", after: 0 }
+    ],
+    [
+      { title: "Modlitwa Mojżesza", after: 11 }
+    ],
+    [
+      { title: "Odnowienie przymierza", after: 0 }
+    ],
+    [
+      { title: "Pouczenie Izraelitów o przepisach kultu", after: 0 }
+    ],
+    [
+      { title: "Budowa przybytku i jego sprzętów", after: 7 }
+    ],
+    [
+    ],
+    [
+    ],
+    [
+      { title: "Wykonanie szat kapłańskich", after: 0 }
+    ],
+    [
+    ]
+  ],
+  "Kpl": [
+    [
+      { title: "Ofiary ze zwierząt", after: 0 },
+      { title: "Ofiara z cielców", after: 2 },
+      { title: "Ofiara z baranków i koziołków", after: 9 },
+      { title: "Ofiara z gołębi", after: 13 }
+    ],
+    [
+      { title: "Ofiary pokarmowe", after: 0 },
+      { title: "Ofiara z ciasta", after: 3 },
+      { title: "Przepisy ogólne", after: 10 },
+    ],
+    [
+      { title: "Cielce", after: 0 },
+      { title: "Owce", after: 5 },
+      { title: "Kozy", after: 11 },
+      { title: "Nie wolno jeść krwi ani tłuszczu zwierząt ofiarnych", after: 16 }
+    ],
+    [
+      { title: "Za grzech arcykapłana", after: 0 },
+      { title: "Za społeczność", after: 12 },
+      { title: "Za naczelnika rodu", after: 21 },
+      { title: "Za zwykłych ludzi", after: 26 }
+    ],
+    [
+      { title: "Szczególne rodzaje ofiar przebłagalnych", after: 0 },
+      { title: "Gołębie jako ofiara ubogich", after: 6 },
+      { title: "Mąka jako ofiara ubogich", after: 10 },
+      { title: "Za przywłaszczenie sobie rzeczy należnych Bogu", after: 13 },
+      { title: "Za nieświadome przestępstwa", after: 16 },
+      { title: "Zadośćuczynienie bliźniemu", after: 19 }
+    ],
+    [
+      { title: "Codzienna ofiara całopalna", after: 0 },
+      { title: "Kapłani otrzymują część z ofiar pokarmowych", after: 6 },
+      { title: "Ofiara pokarmowa przy wprowadzeniu w czynności kapłańskie", after: 11 },
+      { title: "Ofiara przebłagalna", after: 16 }
+    ],
+    [
+      { title: "Ofiara zadośćuczynienia", after: 0 },
+      { title: "Część należna kapłanom z ofiar prywatnych", after: 6 },
+      { title: "Ofiara biesiadna", after: 10 },
+      { title: "Zakaz spożywania tłuszczu i krwi", after: 21 },
+      { title: "Część z ofiar pojednania należna kapłanom", after: 27 },
+      { title: "Zakończenie", after: 34 }
+    ],
+    [
+      { title: "Przygotowania - wprowadzenie Aarona i jego synów w czynności kapłańskie", after: 0 },
+      { title: "Kąpiel, włożenie szat i namaszczenie", after: 5 },
+      { title: "Ofiara przebłagalna za kapłanów", after: 13 },
+      { title: "Ofiara całopalna przy wprowadzeniu", after: 17 },
+      { title: "Obrzędy wprowadzenia w czynności kapłańskie", after: 21 },
+      { title: "Uczta ofiarna i tydzień wprowadzenia w czynności kapłańskie", after: 30 }
+    ],
+    [
+      { title: "Przygotowanie - początek posługi Aarona", after: 0 },
+      { title: "Ofiara za kapłanów", after: 7 },
+      { title: "Ofiara za lud", after: 14 },
+      { title: "Aaron błogosławi, Pan się ukazuje", after: 21 }
+    ],
+    [
+      { title: "Śmierć Nadaba i Abihu", after: 0 },
+      { title: "Mojżesz zabrania Aaronowi żałoby", after: 5 },
+      { title: "Kapłanom nie wolno pić wina podczas służby liturgicznej", after: 7 },
+      { title: "Część z ofiar pokarmowych i ofiar biesiadnych należna kapłanom", after: 11 },
+      { title: "Kozioł ofiary przebłagalnej jako pokarm kapłanów", after: 15 }
+    ],
+    [
+      { title: "Zwierzęta lądowe", after: 0 },
+      { title: "Istoty wodne", after: 8 },
+      { title: "Istoty skrzydlate", after: 12 },
+      { title: "Nieczysty jest każdy, kto dotyka padliny zwierząt nieczystych", after: 23 },
+      { title: "Nieczystość spowodowana przez dotknięcie padliny małych zwierząt", after: 28 },
+      { title: "Padlina czystych zwierząt", after: 38 },
+      { title: "Nie wolno jeść nieczystych małych zwierząt", after: 40 },
+      { title: "Zakończenie przepisów o pokarmach", after: 46 }
+    ],
+    [
+      { title: "Czas oczyszczenia", after: 0 },
+      { title: "Ofiara oczyszczenia matki", after: 5 }
+    ],
+    [
+      { title: "Trąd u ludzi. Pierwszy rodzaj: nabrzmienie, wysypka lub plama", after: 0 },
+      { title: "Drugi rodzaj: ostry trąd", after: 8 },
+      { title: "Trzeci rodzaj: podejrzane wrzody", after: 17 },
+      { title: "Czwarty rodzaj: trąd na oparzeliźnie", after: 23 },
+      { title: "Piąty rodzaj: trąd na głowie lub brodzie", after: 28 },
+      { title: "Szósty rodzaj: pokrzywka", after: 37 },
+      { title: "Siódmy rodzaj: trąd u łysych", after: 39 },
+      { title: "Przepisy dla trędowatych", after: 44 },
+      { title: "Trąd na tkaninach i skórach", after: 46 }
+    ],
+    [
+      { title: "Oczyszczenie po uzdrowieniu z trądu", after: 0 },
+      { title: "Ofiara składana podczas oczyszczenia trędowatego", after: 9 },
+      { title: "Ofiara ubogich", after: 20 },
+      { title: "Trąd na domach", after: 32 },
+      { title: "Oczyszczenie domu", after: 48 },
+      { title: "Zakończenie", after: 32 }
+    ],
+    [
+      { title: "Wycieki u mężczyzn", after: 0 },
+      { title: "Oczyszczenie z wycieków", after: 12 },
+      { title: "Nieczystość z powodu wylewu nasienia", after: 15 },
+      { title: "Nieczystość z powodu krwawienia miesięcznego", after: 18 },
+      { title: "Chorobliwy upływ krwi", after: 24 },
+      { title: "Oczyszczenie kobiety", after: 27 },
+      { title: "Zakończenie", after: 30 }
+    ],
+    [
+      { title: "Przygotowania", after: 0 },
+      { title: "Przyprowadzenie zwierząt ofiarnych", after: 5 },
+      { title: "Przebłaganie za grzechy Aarona", after: 10 },
+      { title: "Przebłaganie za lud", after: 14 },
+      { title: "Kozioł wypędzony na pustynię", after: 19 },
+      { title: "Ofiara całopalna w Dniu Przebłagania", after: 22 },
+      { title: "Dodatkowe upomnienia", after: 28 }
+    ],
+    [
+      { title: "Tylko na jednym miejscu wolno zabijać i składać ofiary", after: 0 },
+      { title: "Nie wolno spożywać krwi", after: 9 },
+      { title: "Nie wolno jeść mięsa zwierząt padłych lub rozszarpanych", after: 14 }
+    ],
+    [
+      { title: "Świętość małżeństwa i rodziny", after: 0 },
+      { title: "Małżeństwa między krewnymi są zakazane", after: 5 },
+      { title: "Czystość małżeńska. Zakaz nadużyć", after: 18 },
+      { title: "Końcowe upomnienie", after: 23 }
+    ],
+    [
+      { title: "Przepisy dotyczące kultu", after: 0 },
+      { title: "Żniwa ubogich", after: 8 },
+      { title: "Obowiązki względem bliźniego", after: 10 },
+      { title: "Zakaz łączenia dwóch różnych gatunków", after: 18 },
+      { title: "Obcowanie z niewolnicą", after: 19 },
+      { title: "Pierwociny drzew owocowych", after: 22 },
+      { title: "Różne zakazy", after: 25 },
+      { title: "Sprawiedliwość społeczna", after: 31 }
+    ],
+    [
+      { title: "Kary za bałwochwalstwo", after: 0 },
+      { title: "Ochrona rodziny i małżeństwa", after: 8 },
+      { title: "Upomnienie końcowe", after: 21 },
+      { title: "Zwierzęta nieczyste. Zakończenie", after: 24 }
+    ],
+    [
+      { title: "Świętość zwykłego kapłana", after: 0 },
+      { title: "Świętość arcykapłana", after: 9 },
+      { title: "Kto jest niezdolny do kapłaństwa", after: 15 }
+    ],
+    [
+      { title: "Święte rzeczy są dla świętych!", after: 0 },
+      { title: "Udział nie-kapłanów w spożywaniu żertw", after: 9 },
+      { title: "Zwierzęta ofiarne mają być bez skazy", after: 16 },
+      { title: "Dodatek: upomnienie końcowe", after: 25 }
+    ],
+    [
+      { title: "Szabat", after: 0 },
+      { title: "Pascha i Święto Przaśników", after: 3 },
+      { title: "Pierwociny", after: 8 },
+      { title: "Święto Tygodni", after: 14 },
+      { title: "Święto nowiu w siódmym miesiącu", after: 22 },
+      { title: "Dzień Przebłagania", after: 25 },
+      { title: "Święto Namiotów czyli \"Kuczek\"", after: 32 }
+    ],
+    [
+      { title: "Oliwa do świecznika", after: 0 },
+      { title: "Chleby pokładne", after: 4 },
+      { title: "Kara za bluźnierstwo", after: 9 },
+      { title: "Kary za uderzenia i za morderstwo", after: 16 }
+    ],
+    [
+      { title: "Rok szabatowy", after: 0 },
+      { title: "Przepisy ogólne dotyczące roku jubileuszowego", after: 7 },
+      { title: "Własność gruntowa w roku jubileuszowym", after: 12 },
+      { title: "Błogosławieństwo na rok szabatowy", after: 17 },
+      { title: "Wykup własności gruntowej", after: 22 },
+      { title: "Przepisy dotyczące domów", after: 28 },
+      { title: "Pomoc dla ubogich rodaków", after: 34 },
+      { title: "Niewolnicy Izraelici", after: 38 },
+      { title: "Obcy niewolnicy", after: 43 },
+      { title: "Izraelici jako niewolnicy u obcych", after: 46 }
+    ],
+    [
+      { title: "Błogosławieństwo ziemi", after: 0 },
+      { title: "Obietnica pokoju i bezpieczeństwa", after: 5 },
+      { title: "Skutki przekleństwa: choroby i wojny", after: 13 },
+      { title: "Posucha i nieurodzaj", after: 17 },
+      { title: "Dzikie zwierzęta", after: 20 },
+      { title: "Wojna, zaraza i głód", after: 22 },
+      { title: "Zagłada i wygnanie", after: 26 },
+      { title: "Kraj będzie zamieniony w pustynię", after: 33 },
+      { title: "Miłosierdzie dla pokutujących", after: 39 }
+    ],
+    [
+      { title: "Oszacowanie ludzi", after: 0 },
+      { title: "Oszacowanie bydła", after: 8 },
+      { title: "Oszacowanie domu", after: 13 },
+      { title: "Oszacowanie własności gruntowej dziedzicznej", after: 15 },
+      { title: "Oszacowanie gruntów kupionych", after: 21 },
+      { title: "Wykup pierworodnych bydła", after: 25 },
+      { title: "Poświęcenie przez \"cherem\"", after: 27 },
+      { title: "Wykup dziesięciny", after: 29 }
+    ],
   ]
 }
 
