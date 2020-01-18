42a686d2b78456e8d533d6b492656be1dda5fb8c
Marta <ptimuleczka@gmail.com>
2020-01-18 22:43:54 +0100

    Add headings for Lb and Mt

--- src/bible/content/addons/headings.js
+++ src/bible/content/addons/headings.js
@@ -510,6 +510,390 @@ var headings = {
       { title: "Poświęcenie przez \"cherem\"", after: 27 },
       { title: "Wykup dziesięciny", after: 29 }
     ]
+  ],
+  "Lb": [
+    [
+      { title: "Pierwszy spis męzczyzn zdolnych do walki", after: 0 },
+      { title: "Imiona wodzów pokoleń", after: 4 },
+      { title: "Dane spisu ludności", after: 19 }
+    ],
+    [
+      { title: "Rozmieszczenie pokoleń w obozie", after: 0 }
+    ],
+    [
+      { title: "Kapłani", after: 0 },
+      { title: "Obowiązki lewitów", after: 4 },
+      { title: "Wybór lewitów", after: 10 },
+      { title: "Rody lewitów", after: 13 },
+      { title: "Lewici w miejsce pierworodnych", after: 39 }
+    ],
+    [
+      { title: "Kehatyci", after: 0 },
+      { title: "Gerszonici", after: 20 },
+      { title: "Meraryci", after: 28 },
+      { title: "Spis lewitów", after: 33 }
+    ],
+    [
+      { title: "Usunięcie nieczystych", after: 0 },
+      { title: "Zwrot dobra nieprawnie zabranego", after: 4 },
+      { title: "Ofiara za posądzenie", after: 10 }
+    ],
+    [
+      { title: "Nazireat", after: 0 }
+    ],
+    [
+      { title: "Dary ofiarne wodzów pokoleń", after: 0 }
+    ],
+    [
+      { title: "Lampy świecznika", after: 0 },
+      { title: "Ofiarowanie lewitów", after: 4 }
+    ],
+    [
+      { title: "Czas Paschy", after: 0 },
+      { title: "Wypadki szczególne", after: 5 },
+      { title: "Obłok jako znak obecności Pana", after: 14 }
+    ],
+    [
+      { title: "Przepisy o trąbach", after: 0 },
+      { title: "Porządek marszu", after: 10 },
+      { title: "Mojżesz i Chobab", after: 28 }
+    ],
+    [
+      { title: "Tabeera", after: 0 },
+      { title: "Narzekanie ludu", after: 3 },
+      { title: "Odpowiedź Pana", after: 15 },
+      { title: "Siedemdziesięciu starszych otrzymuje natchnienie prorockie", after: 23 }
+    ],
+    [
+      { title: "Chaserot. Narzekania Miriam i Aarona", after: 0 }
+    ],
+    [
+      { title: "Wywiad w ziemi Kanaan", after: 0 }
+    ],
+    [
+      { title: "Bunt Izraelitów", after: 0 },
+      { title: "Gniew Pana i wstawiennictwo Mojżesza", after: 9 },
+      { title: "Przebaczenie i kara", after: 19 }
+    ],
+    [
+      { title: "Ofiary z pokarmów i płynów", after: 0 },
+      { title: "Pierwociny chleba", after: 16 },
+      { title: "Wynagrodzenie za grzechy nieuwagi", after: 21 },
+      { title: "Naruszenie szabatu", after: 31 },
+      { title: "Frędzle przy szatach", after: 36 }
+    ],
+    [
+      { title: "Bunt przeciw Mojżeszowi", after: 0 },
+      { title: "Ukaranie buntowników", after: 15 }
+    ],
+    [
+      { title: "Laska Aarona", after: 15 },
+      { title: "Kapłan jako pośrednik pojednania", after: 26 }
+    ],
+    [
+      { title: "Udział kapłanów w darach ofiarnych", after: 7 },
+      { title: "Część należna lewitom", after: 19 },
+      { title: "Dziesięciny", after: 24 }
+    ],
+    [
+      { title: "Popiół z czerwonej krowy", after: 0 },
+      { title: "Poszczególne wypadki zaciągnięcia nieczystości", after: 10 },
+      { title: "Rytualna woda oczyszczenia", after: 16 }
+    ],
+    [
+      { title: "Wody Meriba", after: 0 },
+      { title: "Ukaranie Mojżesza i Aarona", after: 11 },
+      { title: "Edom odmawia Izraelowi przejścia", after: 13 },
+      { title: "Śmierć Aarona", after: 21 }
+    ],
+    [
+      { title: "Zajęcie miasta Chorma", after: 0 },
+      { title: "Jadowite węże", after: 3 },
+      { title: "Marsz w kierunku Zajordania", after: 9 },
+      { title: "Podbój Zajordania", after: 20 }
+    ],
+    [
+      { title: "Król Moabu wzywa Balaama", after: 1 },
+      { title: "Balaam u Balaka", after: 35 }
+    ],
+    [
+      { title: "Przepowiednie Balaama", after: 3 }
+    ],
+    [
+    ],
+    [
+      { title: "Izrael w Peor", after: 0 }
+    ],
+    [
+      { title: "Spis", after: 0 },
+      { title: "Spis lewitów", after: 56 }
+    ],
+    [
+      { title: "Wyposażenie \"córek dziedziczących\"", after: 0 },
+      { title: "Jozue wodzem całej społeczności", after: 10 }
+    ],
+    [
+      { title: "Przepisy o ofiarach", after: 0 },
+      { title: "Ofiary codzienne", after: 2 },
+      { title: "Ofiary w szabat", after: 8 },
+      { title: "Ofiary przy nowiu księżyca", after: 10 },
+      { title: "Ofiary na Święto Paschy", after: 15 },
+      { title: "Ofiary Święta Tygodni", after: 25 }
+    ],
+    [
+      { title: "Ofiary siódmego nowiu", after: 0 },
+      { title: "Ofiary w Dzień Przebłagania", after: 6 },
+      { title: "Święto Namiotów", after: 11 }
+    ],
+    [
+      { title: "Prawo o ślubach", after: 1 }
+    ],
+    [
+      { title: "Wojna święta przeciw Madianitom", after: 0 },
+      { title: "Podział zdobyczy", after: 24 }
+    ],
+    [
+      { title: "Podział Zajordania", after: 0 }
+    ],
+    [
+      { title: "Poszczególne etapy wyjścia", after: 0 },
+      { title: "Boży nakaz co do podziału Kanaanu", after: 49 }
+    ],
+    [
+      { title: "Granice Kanaanu", after: 0 },
+      { title: "Przewodniczący podziału", after: 15 }
+    ],
+    [
+      { title: "Dział lewitów", after: 0 },
+      { title: "Miasta ucieczki", after: 8 }
+    ],
+    [
+      { title: "Dziedzictwo kobiet zamężnych", after: 0 },
+      { title: "Zakończenie", after: 12 }
+    ]
+  ],
+
+  "Mt": [
+    [
+      { title: "Rodowód Jezusa", after: 0 },
+      { title: "Narodzenie Jezusa", after: 17 }
+    ],
+    [
+      { title: "Mędrcy ze Wschodu", after: 0 },
+      { title: "Ucieczka do Egiptu", after: 12 },
+      { title: "Rzeź niemowląt", after: 15 },
+      { title: "Powrót do Nazaretu", after: 18 }
+    ],
+    [
+      { title: "Jan Chrzciciel", after: 0 },
+      { title: "Chrzest Jezusa", after: 12 }
+    ],
+    [
+      { title: "Kuszenie Jezusa", after: 0 },
+      { title: "Pole działalności", after: 11 },
+      { title: "Powołanie pierwszych uczniów", after: 17 },
+      { title: "Dalsza działalność Jezusa", after: 22 }
+    ],
+    [
+      { title: "Osiem błogosławieństw", after: 2 },
+      { title: "Zadanie uczniów", after: 12 },
+      { title: "Jezus a Prawo", after: 16 },
+      { title: "Piąte przykazanie", after: 20 },
+      { title: "Szóste przykazanie", after: 26 },
+      { title: "Ósme przykazanie", after: 32 },
+      { title: "Prawo odwetu", after: 37 },
+      { title: "Miłość nieprzyjaciół", after: 42 }
+    ],
+    [
+      { title: "Czystość zamiarów", after: 0 },
+      { title: "Jałmużna", after: 1 },
+      { title: "Modlitwa", after: 4 },
+      { title: "Post", after: 15 },
+      { title: "Dobra trwałe", after: 18 },
+      { title: "Zbytnie troski", after: 24 }
+    ],
+    [
+      { title: "Powściągliwość w sądzeniu. Obłuda", after: 0 },
+      { title: "Wytrwałość w modlitwie", after: 6 },
+      { title: "Złota zasada postępowania", after: 11 },
+      { title: "Ciasna brama", after: 12 },
+      { title: "Ostrzeżenie przed fałszywymi apostołami", after: 14 },
+      { title: "Łudzenie samego siebie", after: 20 },
+      { title: "Dobra lub zła budowa", after: 23 }
+    ],
+    [
+      { title: "Uzdrowienie trędowatego", after: 0 },
+      { title: "Setnik z Kafarnaum", after: 4 },
+      { title: "W domu Piotra. Liczne uzdrowienia", after: 13 },
+      { title: "Potrzeba wyrzeczenia", after: 17 },
+      { title: "Uciszenie burzy", after: 22 },
+      { title: "Dwaj opętani", after: 27 }
+    ],
+    [
+      { title: "Uzdrowienie paralityka", after: 0 },
+      { title: "Powołanie Mateusza", after: 8 },
+      { title: "Sprawa postów", after: 13 },
+      { title: "Córka Jaira i kobieta cierpiąca na krwotok", after: 17 },
+      { title: "Uzdrowienie dwóch niewidomych", after: 26 },
+      { title: "Uzdrowienie opętanego i chorych", after: 31 }
+    ],
+    [
+      { title: "Wybór Dwunastu", after: 0 },
+      { title: "Mowa misyjna", after: 4 },
+      { title: "Zapowiedź prześladowań", after: 16 },
+      { title: "Męstwo w ucisku", after: 23 },
+      { title: "Za Jezusem lub przeciw Niemu. Wyrzeczenie", after: 33 },
+      { title: "Nagroda za oddanie się Jezusowi", after: 39 }
+    ],
+    [
+      { title: "Poselstwo Jana Chrzciciela", after: 1 },
+      { title: "Świadectwo Jezusa o Janie", after: 6 },
+      { title: "Sąd Jezusa o współczesnych", after: 15 },
+      { title: "Biada nie pokutującym miastom", after: 19 },
+      { title: "Objawienie Ojca i Syna", after: 24 },
+      { title: "Wezwanie do utrudzonych", after: 27 }
+    ],
+    [
+      { title: "Łuskanie kłosów w szabat", after: 0 },
+      { title: "Uzdrowienie w szabat", after: 8 },
+      { title: "Jezus \"Sługa Pański\"", after: 14 },
+      { title: "Zarzut faryzeuszów i obrona Jezusa", after: 21 },
+      { title: "Grzech przeciwko Duchowi Świętemu", after: 30 },
+      { title: "Znak Jonasza", after: 37 },
+      { title: "Nawrót do grzechu", after: 42 },
+      { title: "Prawdziwi krewni Jezusa", after: 45 }
+    ],
+    [
+      { title: "Przypowieść o siewcy", after: 0 },
+      { title: "Cel przypowieści", after: 9 },
+      { title: "Wyjaśnienie przypowieści o siewcy", after: 17 },
+      { title: "Przypowieść o chwaście", after: 23 },
+      { title: "Przypowieść o ziarnku gorczycy i o zaczynie", after: 30 },
+      { title: "Wyjaśnienie przypowieści o chwaście", after: 35 },
+      { title: "Przypowieść o skarbie i perle", after: 43 },
+      { title: "Przypowieść o sieci", after: 46 },
+      { title: "Jezus w Nazarecie", after: 52 }
+    ],
+    [
+      { title: "Ścięcie Jana Chrzciciela", after: 0 },
+      { title: "Pierwsze rozmnożenie chleba", after: 12 },
+      { title: "Jezus chodzi po jeziorze", after: 21 },
+      { title: "Uzdrowienia w Genezaret", after: 33 }
+    ],
+    [
+      { title: "Spór o tradycję", after: 0 },
+      { title: "Prawdziwa nieczystość", after: 9 },
+      { title: "Wiara kobiety kananejskiej", after: 20 },
+      { title: "Uzdrowienia nad jeziorem", after: 28 },
+      { title: "Drugie rozmnożenie chleba", after: 31 }
+    ],
+    [
+      { title: "Nowe żądanie znaku", after: 0 },
+      { title: "Kwas faryzeuszów i saduceuszów", after: 4 },
+      { title: "Wyznanie Piotra", after: 12 },
+      { title: "Pierwsza zapowiedź męki i zmartwychwstania", after: 20 },
+      { title: "Warunki naśladowania Jezusa", after: 23 }
+    ],
+    [
+      { title: "Przemienienie Jezusa", after: 0 },
+      { title: "Przyjście Eliasza", after: 8 },
+      { title: "Uzdrowienie epileptyka", after: 13 },
+      { title: "Druga zapowiedź męki i zmartwychwstania", after: 21 },
+      { title: "Podatek na świątynię", after: 23 }
+    ],
+    [
+      { title: "Spór o pierwszeństwo", after: 0 },
+      { title: "Zgorszenie", after: 5 },
+      { title: "Owca zabłąkana", after: 11 },
+      { title: "Upomnienie braterskie", after: 14 },
+      { title: "Obowiązek przebaczenia", after: 20 },
+      { title: "Nielitościwy dłużnik", after: 22 }
+    ],
+    [
+      { title: "Nierozerwalność małżeństwa", after: 0 },
+      { title: "Dobrowolna bezżenność", after: 9 },
+      { title: "Jezus błogosławi dzieci", after: 12 },
+      { title: "Bogaty młodzieniec", after: 15 },
+      { title: "Niebezpieczeństwo bogactw", after: 22 },
+      { title: "Nagroda za dobrowolne ubóstwo", after: 26 }
+    ],
+    [
+      { title: "Przypowieść o robotnikach w winnicy", after: 0 },
+      { title: "Trzecia zapowiedź męki i zmartwychwstania", after: 17 },
+      { title: "Synowie Zebedeusza", after: 19 },
+      { title: "Przełożeństwo jest służbą", after: 23 },
+      { title: "Niewidomi pod Jerychem", after: 28 }
+    ],
+    [
+      { title: "Uroczysty wjazd do Jerozolimy", after: 0 },
+      { title: "Jezus w świątyni", after: 11 },
+      { title: "Nieurodzajne drzewo figowe", after: 17 },
+      { title: "Pytanie o władzę", after: 22 },
+      { title: "Przypowieść o dwóch synachw", after: 27 },
+      { title: "Przypowieść o przewrotnych rolnikach", after: 32 }
+    ],
+    [
+      { title: "Przypowieść o uczcie królewskiej", after: 0 },
+      { title: "Sprawa podatku", after: 14 },
+      { title: "Sprawa zmartwychwstania", after: 22 },
+      { title: "Największe przykazanie", after: 33 },
+      { title: "Mesjasz Synem Bożym", after: 40 }
+    ],
+    [
+      { title: "Ostrzeżenie przed uczonymi w Piśmie", after: 0 },
+      { title: "Biada obłudnikom", after: 12 },
+      { title: "Jezus odwraca się od Miasta", after: 36 }
+    ],
+    [
+      { title: "Zniszczenie świątyni", after: 0 },
+      { title: "Początek boleści", after: 3 },
+      { title: "Prześladowanie uczniów", after: 8 },
+      { title: "Znaki zapowiadające zburzenie Jerozolimy", after: 14 },
+      { title: "Przyjście Chrystusa", after: 22 },
+      { title: "Przykład z drzewa figowego", after: 31 },
+      { title: "Nieznany czas przyjścia", after: 35 },
+      { title: "Potrzeba czujności", after: 41 },
+      { title: "Przypowieść o słudze wiernym i niewiernym", after: 44 }
+    ],
+    [
+      { title: "Przypowieść o pannach roztropnych i nierozsądnych", after: 0 },
+      { title: "Przypowieść o talentach", after: 13 },
+      { title: "Sąd Ostateczny", after: 30 }
+    ],
+    [
+      { title: "Ostatnia zapowiedź męki", after: 0 },
+      { title: "Postanowienie Wysokiej Rady", after: 23 },
+      { title: "Namaszczenie w Betanii", after: 5 },
+      { title: "Zdrada Judasza", after: 13 },
+      { title: "Przygotowanie Paschy", after: 16 },
+      { title: "Wyjawienie zdrajcy", after: 19 },
+      { title: "Ustanowienie Eucharystii", after: 25 },
+      { title: "Przepowiednia zaparcia się Piotra", after: 30 },
+      { title: "Modlitwa i trwoga konania", after: 35 },
+      { title: "Pojmanie Jezusa", after: 46 },
+      { title: "Wobec Wysokiej Rady", after: 56 },
+      { title: "Zaparcie się Piotra", after: 68 }
+    ],
+    [
+      { title: "Wydanie Jezusa Piłatowi", after: 0 },
+      { title: "Koniec zdrajcy", after: 2 },
+      { title: "Jezus przed Piłatem", after: 10 },
+      { title: "Jezus odrzucony przez swój naród", after: 14 },
+      { title: "Król wyśmiany", after: 26 },
+      { title: "Droga krzyżowa", after: 31 },
+      { title: "Ukrzyżowanie", after: 34 },
+      { title: "Wyszydzenie na krzyżu", after: 38 },
+      { title: "Modlitwa i trwoga konania", after: 44 },
+      { title: "Po śmierci Jezusa", after: 50 },
+      { title: "Pogrzeb Jezusa", after: 56 },
+      { title: "Straż przy grobie", after: 61 }
+    ],
+    [
+      { title: "Pusty grób", after: 0 },
+      { title: "Jezus ukazuje się niewiastom", after: 8 },
+      { title: "Przekupiona straż", after: 10 },
+      { title: "Ostatni rozkaz", after: 15 }
+    ]
   ]
 }
 
